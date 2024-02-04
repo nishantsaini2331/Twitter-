7ed8caea0e4093d219ddb3ab0ae1419764d4177b
Nishant Saini <ns0109375@gmail.com>
2024-02-04 13:15:20 +0530

    model

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const dbConnect = require("./config/dbConnect");
+const User = require("./model/userModel");
 const app = express();
 dotenv.config();
 app.use(express.json());
 
 const PORT = process.env.PORT
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
+
+
     console.log("Server started")
     dbConnect();
+
+
+    await User.create({
+        name:"nishant",
+        email:"123@gmaill.com"
+    })
 });
---
+++ model/userModel.js
@@ -0,0 +1,13 @@
+const { model } = require("mongoose");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+    },
+    email: {
+        type: String,
+    },
+});
+
+module.exports = model("User", userSchema);
